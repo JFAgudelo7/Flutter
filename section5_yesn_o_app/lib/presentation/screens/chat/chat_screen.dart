88c0bae34b142b74761b8da04ed4e8f9ba7038f7
Juan Felipe Agudelo Arboleda <jagudelo9@gmail.com>
2026-02-19 22:01:58 -0500

    Section 5 - End

--- section5_yesn_o_app/lib/presentation/screens/chat/chat_screen.dart
+++ section5_yesn_o_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:section5_yesn_o_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:section5_yesn_o_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:section5_yesn_o_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -42,7 +43,8 @@ class _ChatView extends StatelessWidget {
                   : const MyMessageBubble();
                 }
               ),
-            )
+            ),
+            const MessageFieldBox(),
             ],
         ),
       ),
