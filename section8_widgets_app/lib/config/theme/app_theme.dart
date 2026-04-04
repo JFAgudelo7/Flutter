b72dbd21ca99d5e7bf9fc4949146c6f41fc2abcd
Juan Felipe Agudelo Arboleda <jagudelo9@gmail.com>
2026-04-04 10:30:45 -0500

    Section 9 - WidgetApp is section 9 not 8, adding Theme

---
+++ section8_widgets_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.pink,
+  Colors.pinkAccent
+];
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }): assert(selectedColor >= 0, 'Selected Color must be greater than 0'),
+      assert(selectedColor < colorList.length, 'Selected Color must be less or equal to ${colorList.length}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[ selectedColor ],
+  );
+}
