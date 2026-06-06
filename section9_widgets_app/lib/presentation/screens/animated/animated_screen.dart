41753432aa79d77c1d4ff30496fee7f6eccf5b3f
Juan Felipe Agudelo Arboleda <jagudelo9@gmail.com>
2026-06-06 10:26:18 -0500

    Section 10 - Animated Container

--- section9_widgets_app/lib/presentation/screens/animated/animated_screen.dart
+++ section9_widgets_app/lib/presentation/screens/animated/animated_screen.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget{
+class AnimatedScreen extends StatefulWidget{
   static const name = 'animated_screen';
 
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen>{
+
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10;
+
+   void changeShape(){
+    final random = Random();
+    width = random.nextInt(300) + 50;
+    height = random.nextInt(500) + 50;
+    borderRadius = random.nextInt(100) + 10;
+    color = Color.fromRGBO(
+      random.nextInt(255),  //Red
+      random.nextInt(255),  //Green
+      random.nextInt(255),  //Blue
+      1                     //Opacity
+    );
+
+    setState(() {});
+   }
 
   @override
   Widget build(BuildContext context){
@@ -15,22 +42,20 @@ class AnimatedScreen extends StatelessWidget{
       body: Center(
         child: AnimatedContainer(
           duration: const Duration( milliseconds: 400),
-          width: 100,
-          curve: Curves.easeOutCubic,
-          height: 400,
+          width: width <= 0 ? 0 : width,
+          curve: Curves.elasticOut,
+          height: height <= 0 ? 0 : height,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20),
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius < 0 ? 0 : borderRadius),
           ),
         ),
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
-        },
+        onPressed: changeShape,
         child: const Icon( Icons.play_arrow_rounded),
       )
     );
   }
-  }
+}
