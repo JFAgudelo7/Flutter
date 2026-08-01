bc55ff9620e7e8f5a06a5a95aa83831d6a07f0ff
Juan Felipe Agudelo Arboleda <jagudelo9@gmail.com>
2026-08-01 15:37:38 -0500

    Section 12 - Start: Adding Router and Theme

---
+++ section12_cinemapedia/README.md
@@ -0,0 +1,3 @@
+# section12_cinemapedia
+
+A new Flutter project.
---
+++ section12_cinemapedia/android/app/src/main/kotlin/com/example/section12_cinemapedia/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.section12_cinemapedia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
