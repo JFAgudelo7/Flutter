f6eb336179eba3d8c1f494ab5aae9ef0150ebf78
Juan Felipe Agudelo Arboleda <jagudelo9@gmail.com>
2026-09-20 21:31:52 -0500

    Section 12 - Listen Multiple Provider simultaneously

--- section12_cinemapedia/lib/presentation/providers/providers.dart
+++ section12_cinemapedia/lib/presentation/providers/providers.dart
@@ -3,3 +3,4 @@
 export 'movies_providers.dart';
 export 'movies_repository_provider.dart';
 export 'movies_slideshow_provider.dart';
+export 'initial_loading_provider.dart';
--- section12_cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ section12_cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -38,13 +38,16 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
+
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if (initialLoading) return const FullScreenLoader();
+
+
     final slideShowsMovies = ref.watch(moviesSlideShowProvider);
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final popularMovies = ref.watch(popularMoviesProvider);
     final topRatedMovies = ref.watch(topRatedMoviesProvider);
     final upcomigMovies = ref.watch(upcomingMoviesProvider);
-
-    return const FullScreenLoader();
 
     return CustomScrollView(
       slivers: [
